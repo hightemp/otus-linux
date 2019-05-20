3e9ad65c213352e0322eca3db5935c275f2c881c
Anton Panov <hightemp.unknown@gmail.com>
2019-05-21 01:04:55 +0300

    update

--- files/lesson5/ps_ax.sh
+++ files/lesson5/ps_ax.sh
@@ -9,12 +9,16 @@ while [[ $# -gt 0 ]]; do
 			shift
 			shift
 		;;
+		*)
+			echo "Wrong arguments"
+			exit 1
+		;;
 	esac
 done
 
 iCounter=0
 
-sFormat='%5s %-9s%-7s%5s %-100s\n'
+sFormat='%5s %-9s%-7s%8s %-100s\n'
 
 printf "$sFormat" PID TTY STAT TIME COMMAND
 
@@ -27,11 +31,57 @@ for sProccessDir in `ls /proc | sort -V`; do
 		iPID=${BASH_REMATCH[1]}
 		
 		sProcStat=`cat /proc/$iPID/stat`
-
 		sProcState=`echo "$sProcStat" | awk '{print $3}'`
 		sProcTTY=`echo "$sProcStat" | awk '{print $7}'`
-		sProcStartTime=`echo "$sProcStat" | awk '{print $22}'`
-		sProcCommand=`cat /proc/$iPID/cmdline | sed -E 's/[\x0\n]//g'`
+		sProcUTime=`echo "$sProcStat" | awk '{print $14}'`
+		sProcSTime=`echo "$sProcStat" | awk '{print $15}'`
+		sProcCommand=`cat /proc/$iPID/cmdline | sed -E 's/[\x0\n]/ /g'`
+		sProcPriority=`echo "$sProcStat" | awk '{print $18}'`
+		sProcRSS=`echo "$sProcStat" | awk '{print $24}'`
+		sProcStatus=`cat /proc/$iPID/status`
+
+		sProcTTYName='?'
+
+		if [ $sProcTTY -gt 0 ]; then
+			iMAJOR=$(( ($sProcTTY >> 8) & 0xfff ))
+			iMINOR=$(( ($sProcTTY & 0xff) | ($sProcTTY & 0xfff00000) >> 12 ))
+
+			case $iMAJOR in
+				3)
+					if [ $iMINOR -lt 255 ]; then
+						sString0="pqrstuvwxyzabcde"
+						sString1="0123456789abcdef"
+						iTmpMin0=$(( $iMINOR >> 4 ))
+						iTmpMin1=$(( $iMINOR & 0x0f ))
+						sT0="${sString0[$iTmpMin0]}"
+						sT1="${sString1[$iTmpMin1]}"
+
+						sProcTTYName=`printf "/dev/tty%c%c" $sT0 $sT1`
+					fi
+				;;
+			esac
+		fi
+
+		sVmLock=0
+
+		if [[ $sProcStatus =~ VmLck:[[:space:]]+([0-9]+) ]]; then
+			sVmLock=${BASH_REMATCH[1]}
+			if [ $sVmLock -gt 0 ]; then
+				sProcState+="L"
+			fi
+		fi
+
+		if [ "$sProcPriority" -lt 0 ]; then
+			sProcState+="<"
+		fi
+		
+		if [ "$sProcPriority" -gt 0 ]; then
+			sProcState+="N"
+		fi
+
+		if [ "$sProcRss" == "0" -a "$sProcStat" != 'Z' ]; then
+			sProcState+="W"
+		fi
 
 		if [ "$sProcCommand" == "" ]; then
 			sProcCommand="[";
@@ -39,24 +89,28 @@ for sProccessDir in `ls /proc | sort -V`; do
 			sProcCommand+="]"
 		fi
 
-		d=$((sProcStartTime/60/60/24))
-    	h=$((sProcStartTime/60/60%24))
-    	m=$((sProcStartTime/60%60))
-    	s=$((sProcStartTime%60))
+		iClockTicks=$(getconf CLK_TCK)
+
+		(( sProcTime=$sProcUTime+$sProcSTime/$iClockTicks ))
 		
+		(( ss=$sProcTime%60 ))
+		(( sProcTime/=60 ))
+		(( mm=$sProcTime%60 ))
+		(( sProcTime/=60 ))
+		(( hh=$sProcTime%60 ))
+		(( sProcTime/=60 ))
+		(( dd=$sProcTime%60 ))
+		(( sProcTime/=60 ))
+
 		sTime=""
 		
-		if [[ $d > 0 ]]; then
-			sTime+="$d "
+		if [[ $dd > 0 ]]; then
+			sTime+="$d-"
 		fi
 
-		if [[ $h > 0 ]]; then
-			sTime+="$h "
-		fi
+		sTime=`printf "%02u:%02u:%02u" $hh $mm $ss`
 
-		sTime=`printf "%02d:%02d" $m $s`
-
-		printf "$sFormat" "$iPID" "$sProcTTY" "$sProcState" "$sTime" "$sProcCommand"
+		printf "$sFormat" "$iPID" "$sProcTTYName" "$sProcState" "$sTime" "$sProcCommand"
 	fi
 
 	(( iCounter++ ))
